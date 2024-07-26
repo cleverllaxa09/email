3e83847334eaa663a8605528cdb17229622fbc73
cleverllaxa09 <josephvincentcayabyablaxa@gmail.com>
2024-07-26 18:22:40 +0800

    2 commit

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,21 @@ use App\Http\Controllers\PDFController;
 Route::get('/', function () {
     return 'main Page';
 });
-Route::get('/hello', function () {
+Route::get('/xxx', function () {
     return 'HeeLLo';
 })->name('hello');
 Route::get('/greet/{name}', function ($name) {
     return 'Hello '. $name .' !';
 });
 Route::get('/hallo', function () {
-    return redirect('/hello');
+    return redirect()->route('hello');
 });
+Route::fallback(function () {
 
+    return 'wala kang route';
+}
 
+);
 Route::get('generate-pdf', [PDFController::class, 'generatePDF']);
 
 Route::get('send-email-pdf', [PDFController::class, 'index']);
